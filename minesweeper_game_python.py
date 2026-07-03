83fd2852561d5d2f30c9ed79331d5e05c7b5b049
Musa Shah <shadowvortex604@gmail.com>
2026-07-04 00:49:53 +0500

    Add files via upload

---
+++ minesweeper_game_python.py
@@ -0,0 +1,76 @@
+#Minesweeper Game In Python
+import random
+#board user can NOT SEE(solution)
+board=[[0,0,0,0,0],  #0 =no bomb
+       [0,0,0,0,0],  #1 =bomb
+       [0,0,0,0,0],
+       [0,0,0,0,0],
+       [0,0,0,0,0]]
+#board user can SEE
+boardDisplay=[[-1,-1,-1,-1,-1], #-1= unknown
+       [-1,-1,-1,-1,-1],
+       [-1,-1,-1,-1,-1],
+       [-1,-1,-1,-1,-1],
+       [-1,-1,-1,-1,-1]]
+
+def checkMineAround(row,column):
+    t=0
+    i=row-1
+    while i<= row+1:
+        if i>=0 and i<5:
+            j=column-1
+            while j<=column+1:
+                if j>=0 and j<5:
+                    t=t+board[i][j]
+                j=j+1
+        i=i+1
+    return t
+
+#add Mines
+numMine = 0
+while numMine < 1 or numMine > 25:
+    numMine = int(input("How many mines? (1 to 25): "))
+    if numMine < 1 or numMine > 25:
+        print("Impossible! Please enter a number between 1 and 25.")
+
+print("You selected " + str(numMine) + " mines.")
+num=0
+
+while num < numMine:
+    row = random.randint(0,4) #build-in module 
+    column = random.randint(0,4) #build-in module 
+    if board[row][column] == 0:
+        board[row][column] = 1 #add a mine
+        num += 1 #incrementation
+
+def displaySol():  #function to show solution
+    for row in range(0,5): #For loop(row)
+        for column in range(0,5):
+            print(board[row][column], end=" ") #end is use to stay on the line
+        print(" ") #This is for next line
+
+def displayBoard():
+    print("-"*21)   
+    for row in range(0,5):
+        for column in range(0,5):
+            print("| ", end="")
+            if boardDisplay[row][column] == -1:
+                print(" ", end=" ")
+            else:
+                print(board[row][column], end=" ")
+        print("|")   
+        print("-"*21) 
+
+displaySol()
+displayBoard()
+
+guess=0
+while guess < (25 - numMine):
+    row=int(input("Guess a row(1-5): "))-1
+    column=int(input("Guess a column(1-5): "))-1
+    if board[row][column] == 1:
+        print("Booom!!!! You hit a mine. ")
+        displaySol()
+    else:
+        boardDisplay[row][column]=checkMineAround(row,column)
+        displayBoard()
